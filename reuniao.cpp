3b87e3e83069f2c258d7f46b69b68aa57e232a3e
Dênis de Souza <denissouza9900@gmail.com>
2022-08-07 16:53:45 -0300

    Adicionado o arquivo reuniao

---
+++ reuniao.cpp
@@ -0,0 +1,103 @@
+/*
+    Dênis de Souza Cordeiro - 202110235
+    Gabriel Zanda
+    Ronald de Souza Galdino - 
+*/
+#include<iostream>
+#include<vector>
+#include<utility>
+#include<functional>
+using namespace std;
+
+#define INF 1000000000
+
+/*
+ * Variaveis globais
+ */
+
+// matriz de adjacencia
+int** MA;
+
+// quantidade de vertices
+int n;
+
+// quantidade de arestas
+int m;
+
+// quantidade de pares origem-destino a ser calculado
+int k;
+
+int getMenor(int vetor[]){
+    int menor = vetor[0];
+    for(int i = 0; i < n - 1; i++){
+        if(vetor[i] < menor){
+            menor = vetor[i];
+        }
+    }
+    return menor;
+}
+
+void preencherVertor(int vetor[], int tamanho){
+
+    for(int i = 0; i < tamanho; i++){
+        vetor[i] = 0;
+    }
+}
+
+int floyd_warshall(){
+    for(int k = 0; k < n; k++)
+      for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+          MA[i][j] = min(MA[i][j], MA[i][k] + MA[k][j]);
+}
+
+void imprimirMA(){
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            cout << MA[i][j] << " ";
+        }
+    }
+}
+
+int main(){
+
+    cin >> n >> m;
+        
+    MA = new int*[n];
+    int u, v, w;
+    
+    for(int i = 0; i < n; i++){
+        MA[i] = new int[n];
+        for(int j = 0; j < n; j++)
+            MA[i][j] = INF;
+    }
+
+    for(int i = 0; i < m; i++){
+        cin >> u >> v >> w;
+        MA[u][v] = MA[v][u] = w;
+    }
+    
+    floyd_warshall();
+
+    int resultados[n];
+    preencherVertor(resultados, n);
+
+    int maior;
+    for(int i = 0; i < n; i++){
+        maior = resultados[i];
+        for(int j = 0; j < m; j++){
+            if(i!= j){
+                if(MA[i][j] > maior and MA[i][j] != 0){
+                    maior = MA[i][j];
+                }    
+            }
+        }
+        resultados[i] = maior;
+    }
+
+    int resultado = getMenor(resultados);
+    cout << resultado << endl;
+
+
+    return 0;
+}

a687c4fc290716886ca04990d1fdd8a1443d7eec
Ronald <ronalddesouzagaldino@gmail.com>
2022-09-08 15:23:15 -0300

    Caminho bom

--- CaminhoBom.cpp
+++ CaminhoBom.cpp
@@ -1,126 +1,159 @@
 /*
 Dênis de Souza Cordeiro - 202110235 - 14A
 Gabriel Fernando Zanda Gonçalves - 202110234 - 14A
-Ronald de Souza Galdino - 202110679 - 14A
-*/
-
-/*
-Ideia geral do algoritmo:
-*/
-
-/*
-Observações:
-- Usar Dijkstra (já que não temos arestas negativas, e consideramos um grafo ponderado).
-- Não pode ser FLOYD Warshall pois a quantidade de Vértices pode ultrapassar 400. 
-- Os vértice COMEÇAM EM ZERO.
+Ronald Souza Galdino - 202110679 - 14A
 */
 
 #include<iostream>
 #include<vector>
 #include<queue>
 #include<utility>
-#include<functional>
+#include<algorithm>
 using namespace std;
 
-/*
- * Variaveis globais
- */
+#define INF 10000000
+#define NIL -1
 
-// lista de adjacencia
-vector<pair<int, double>>* LA;
+vector<pair<int, int>>* adj;
+vector<int> caminhos;
+int pai[0];
 
-// numero de vertices do grafo
-int n;
+//Função para registrar o caminho percorrido
+void registrarCaminho(int start, int end){
+    if(pai[start] != end){
+        caminhos.push_back(pai[start]);
+        registrarCaminho(pai[start], end);
+    }
+    else{
+        caminhos.push_back(end);
+    }
+}
 
-// numeor de arestas do grafo
-int m;
+// Função para realizar a remoção das arestas de acordo como fornecido
+void removerArestas(int start, int end){
+    while(pai[end] != NIL){//enquanto o pai da origem não for encontrado
 
-// distancia da origem "org" a cada vertice do grafo
-vector<double> d;
+        for(unsigned i = 0; i < adj[pai[end]].size(); i++){ // percorre as arestas que ligam cada vértice
 
-int dijkstra(int org)
-{
-    d.assign(n, 1.0);
-    
-    // a distance da origem "org" eh sempre zero
-    d[org] = 0;
-    
-    // heap que auxilia na obtencao do vertice com maior prioridade, a cada iteracao
-    priority_queue< pair<double, int>, vector<pair<double, int>>, greater<pair<double, int>> > heap;
+            pair<int, int> aresta = adj[pai[end]][i];
 
-    // primeiro par inserido na heap: "org" com custo zero
-    heap.push(make_pair(0, org));         // ERA 1
- 
-    vector<bool> visitado;
-    visitado.assign(n, false);
- 
-    // o algoritmo para quando a heap estiver vazia
-    while(!heap.empty())
-    {
-        pair<double, int> vertice = heap.top();
-        heap.pop();
-
-        double distancia = vertice.first;
-        int u = vertice.second;
-        cout << "Distancia: " << distancia << " u: " << u << endl;
-     
-        if(visitado[u]) // "u" jah foi explorado
-          continue;
-     
-        visitado[u] = true;
-     
-        double custo;
-        for(int j = 0; j < (int) LA[u].size(); j++)
-        {
-            pair<int, double> vizinho = LA[u][j];
-            int v = vizinho.first;
-            double prob = vizinho.second;
-         
-            // tentativa de melhorar a estimativa de menor caminho da origem ao vertice v
-            custo = d[u] * prob;
-            if(custo < d[v]) 
-            { 
-                d[v] = custo; 
-                heap.push(make_pair(d[v], v)); 
+            if(end == aresta.first){// se for igual
+                adj[pai[end]].erase(adj[pai[end]].begin()+i); // removo a aresta da lista de adj
+                end = pai[end];
+                break;
             }
         }
     }
 }
 
-int main()
-{
-    cin >> n >> m;
-   
-    LA = new vector<pair<int, double>>[n];
-    int u, v;
-    double p;
-    // Entrada das arestas do grafo
-    for(int i = 0; i < m; i++)
-    {
-        cin >> u >> v; 
-        cin >> p;
-        u--;
-        v--;
-        LA[u].push_back(make_pair(++v, p)); // Acrescentei "++" AQUI
-    }
- 
-    int x = 0;
+int dijkstra(int n, int s, int t){
+    // vetor de distâncias
+    int dist[n];
 
-    for(int i = 0; i < m; i++)
-    {
-        
-        cout << "vertice " << ++x << " para : ";
-        for(int j = 0; j < (int) LA[i].size(); j++)
-        {
-            cout << LA[i][j].first << " (" << LA[i][j].second << ") ";
+    //vetor de visitados serve para caso o vértice já tenha sido
+    // expandido (visitado), não expandir mais
+    bool visitados[n];
+
+    // vetor de pais
+    pai[n];
+
+    // fila de prioridades de pair (distancia, vértice)
+    priority_queue < pair<int, int>,vector<pair<int, int> >, greater<pair<int, int> > > heap;
+
+    // inicia o vetor de distâncias, visitados e de pais
+    for(int i = 0; i < n; i++){
+        dist[i] = INF;
+        visitados[i] = false;
+        pai[i] = NIL;
+    }
+
+    // distância da origem para origem é 0 
+    dist[s] = 0;
+
+    //Origem é seu próprio Pai
+    pai[s] = NIL;
+
+    // insere na fila
+    heap.push(make_pair(dist[s], s));
+
+    // loop do algoritmo
+    while(!heap.empty()){
+
+        pair<int, int> p = heap.top(); // extrai o pair do topo
+        int u = p.second; // obtém o vértice do pair
+        heap.pop(); // remove da fila
+
+        // verifica se o vértice não foi VISITADO
+        if(visitados[u] == false){
+
+            // marca como visitado
+            visitados[u] = true;
+
+            // percorre os vértices "v" adjacentes de "u"
+            for(auto it = adj[u].begin(); it != adj[u].end(); it++){
+                int v = it->first; // vértice adjacente
+                int p = it->second; // custo da aresta
+
+                // relaxamento (u, v)
+                if(dist[v] > (dist[u] + p) and visitados[v] == false){    //Adicioneu o "and" na condição.             
+                    // atualiza a distância de "v" e insere na fila
+                    dist[v] = dist[u] + p;
+                    heap.push(make_pair(dist[v], v));
+                    pai[v] = u;
+                    //cout << "pai de " << v << " : " << pai[v] << endl;
+                }
+            }
         }
-        cout << endl;
     }
+    if(dist[t] != INF){
+        caminhos.push_back(t);
+        registrarCaminho(t,s);
+    }
+    return dist[t];
+}
 
-    dijkstra(0);
- 
-    for(int i = 0; i < n; i++)
-      cout << "d[" << i + 1 << "]: " << d[i] << endl;
+int main(){
+    int n, m; // num de vértices e arestas
+    int s, t; // origem e destino
+    int u, v, p; //  pontos u e v da aresta com distância p
 
+    cin >> n >> m;
+     while(n != 0 and m != 0){
+        cin >> s >> t;
+
+        adj = new vector<pair<int, int> >[n];
+
+        // leitura do grafo
+        for(int i = 0; i < m; i++){
+            cin >> u >> v >> p;
+            adj[u].push_back(make_pair(v, p));
+        }
+        int menor_caminho  = dijkstra(n,s,t);
+
+        cout << "Menor Caminho (R) : " << menor_caminho << endl;
+
+
+        // inversão do caminho
+        reverse(caminhos.begin(), caminhos.end());
+
+
+        int caminho2 = menor_caminho;
+        for(int i = 0; i < caminhos.size(); i++){
+            if(caminho2 == menor_caminho and i < caminhos.size()-1){
+                removerArestas(caminhos[i],caminhos[i+1]);
+                caminho2 = dijkstra(n,s,t);
+            }
+        }
+
+        cout << "Segundo menor caminho: ";
+        if(caminho2 != INF){ // verifica se foi achado um segundo caminho
+            cout << caminho2 << endl;
+        } else{
+            cout << -1 << endl;
+        }
+        caminhos.clear();
+
+        cin >> n >> m;
+    }
     return 0;
 }
--- CampoDeTestes.cpp
+++ CampoDeTestes.cpp
@@ -1,69 +1,182 @@
 #include<iostream>
+#include<vector>
+#include<queue>
 #include<utility>
+#include<algorithm>
 using namespace std;
 
-#define INF 1000000000 // Usando a sua definição de "infinito"
+#define INF 10000000
+#define NIL -1
 
-int** MA;   // Matriz de Adjacência
+vector<pair<int, int> > * adj;
+vector<int> caminhos;
+int pai[0];
+//vector<pair<int, int>>* caminhos2;
 
-int miniDist(int distance[], bool Tset[]) // finding minimum distance
-{
-    int minimum=INF; // Mudei aqui
-    int dist;
-              
-    for(int k=0;k<6;k++) 
-    {
-        if(Tset[k]==false && distance[k]<=minimum)      
-        {
-            minimum=distance[k];
-            dist=k;
-        }
+//Função para registrar o caminho percorrido
+void registrarCaminho(int start, int end){
+    //cout << "start : " << start << "end : "<< end << endl;
+    if(pai[start] != end){
+        caminhos.push_back(pai[start]);
+        //caminhos2[0].push_back(make_pair( pai[start], start));
+        registrarCaminho(pai[start], end);
     }
-    return dist;
-}
-
-void DijkstraAlgo(int graph[6][6],int src) // adjacency matrix 
-{
-    int distance[6]; // // array to calculate the minimum distance for each node                             
-    bool Tset[6];// boolean array to mark visited and unvisited for each node
-    
-    // Inicialmente todos os vértices NÃO foram visitados e tem DISTÂNCIA = INFINITO 
-    for(int k = 0; k<6; k++)
-    {
-        distance[k] = INF; //Mudei aqui
-        Tset[k] = false;    
-    }
-    
-    distance[src] = 0;   // Source vertex distance is set 0               
-    
-    for(int k = 0; k<6; k++)                           
-    {
-        int m=miniDist(distance,Tset); 
-        Tset[m]=true;
-        for(int k = 0; k<6; k++)                  
-        {
-            // updating the distance of neighbouring vertex
-            if(!Tset[k] && graph[m][k] && distance[m]!=INF && distance[m]+graph[m][k]<distance[k])
-                distance[k]=distance[m]+graph[m][k];
-        }
-    }
-    cout<<"Vertex\t\tDistance from source vertex"<<endl;
-    for(int k = 0; k<6; k++)                      
-    { 
-        char str=65+k; 
-        cout<<str<<"\t\t\t"<<distance[k]<<endl;
+    else{
+        caminhos.push_back(end);
     }
 }
 
-int main()
-{
-    int graph[6][6]={
-        {0, 1, 2, 0, 0, 0},
-        {1, 0, 0, 5, 1, 0},
-        {2, 0, 0, 2, 3, 0},
-        {0, 5, 2, 0, 2, 2},
-        {0, 1, 3, 2, 0, 1},
-        {0, 0, 0, 2, 1, 0}};
-    DijkstraAlgo(graph,0);
-    return 0;                           
+// Função para realizar a remoção das arestas de acordo como fornecido
+void removerArestas(int start, int end){
+    while(pai[end] != NIL){//enquanto o pai da origem não for encontrado
+        cout << "cheguei no while" << endl;
+        for(unsigned i = 0; i < adj[pai[end]].size(); i++){ // percorre as arestas que ligam cada vértice
+             cout << "cheguei no for" << endl;
+            pair<int, int> aresta = adj[pai[end]][i];
+            if(end == aresta.first){// se for igual
+                 cout << "cheguei no if" << endl;
+                 cout << "end e aresta" << end << " " << aresta.first << endl;
+                adj[pai[end]].erase(adj[pai[end]].begin()+i); // removo a aresta da lista de adj
+                end = pai[end];
+                break;
+            }
+        }
+    }
+}
+
+int dijkstra(int n, int s, int t){
+    // vetor de distâncias
+    int dist[n];
+
+    //vetor de visitados serve para caso o vértice já tenha sido
+    // expandido (visitado), não expandir mais
+    bool visitados[n];
+
+    // vetor de pais
+    pai[n];
+
+    // fila de prioridades de pair (distancia, vértice)
+    priority_queue < pair<int, int>,vector<pair<int, int> >, greater<pair<int, int> > > heap;
+
+    // inicia o vetor de distâncias, visitados e de pais
+    for(int i = 0; i < n; i++){
+        dist[i] = INF;
+        visitados[i] = false;
+        pai[i] = NIL;
+    }
+
+    // distância da origem para origem é 0 
+    dist[s] = 0;
+
+    //Origem é seu próprio Pai
+    pai[s] = NIL;
+
+    // insere na fila
+    heap.push(make_pair(dist[s], s));
+
+    // loop do algoritmo
+    while(!heap.empty()){
+
+        pair<int, int> p = heap.top(); // extrai o pair do topo
+        int u = p.second; // obtém o vértice do pair
+        heap.pop(); // remove da fila
+
+        // verifica se o vértice não foi VISITADO
+        if(visitados[u] == false){
+
+            // marca como visitado
+            visitados[u] = true;
+
+            // percorre os vértices "v" adjacentes de "u"
+            for(auto it = adj[u].begin(); it != adj[u].end(); it++){
+                int v = it->first; // vértice adjacente
+                int p = it->second; // custo da aresta
+
+                // relaxamento (u, v)
+                if(dist[v] > (dist[u] + p) and visitados[v] == false){    //Adicioneu o "and" na condição.             
+                    // atualiza a distância de "v" e insere na fila
+                    dist[v] = dist[u] + p;
+                    heap.push(make_pair(dist[v], v));
+                    pai[v] = u;
+                    //cout << "pai de " << v << " : " << pai[v] << endl;
+                }
+            }
+        }
+    }
+    if(dist[t] != INF){
+        //cout << t << s << endl;
+        caminhos.push_back(t);
+        registrarCaminho(t,s);
+       // removerArestas(t,s,pai);
+    }
+    return dist[t];
+}
+
+int main(){
+    int n, m; // num de vértices e arestas
+    int s, t; // origem e destino
+    int u, v, p; //  pontos u e v da aresta com distância p
+
+    cin >> n >> m;
+    cin >> s >> t;
+
+    adj = new vector<pair<int, int> >[n];
+    //caminhos2 = new vector<pair<int, int> >[n];
+
+    // leitura do grafo
+    for(int i = 0; i < m; i++){
+        cin >> u >> v >> p;
+        adj[u].push_back(make_pair(v, p));
+    }
+    int menor_caminho  = dijkstra(n,s,t);
+
+    cout << "Menor Caminho (R) : " << menor_caminho << endl;
+
+    cout << "Tamanho do Caminho : " << caminhos.size() << endl;
+
+    // inversão do caminho
+    reverse(caminhos.begin(), caminhos.end());
+
+    // Print do Vector de Caminhos
+    cout << "Caminho: ";
+    for(int x : caminhos){
+        cout << x << ' ';
+    }
+
+     cout << endl << "L.A antes: " << endl;
+
+    for(int i = 0; i< n; i++){
+        cout << "Vértice " << i << " : ";
+        for(auto it = adj[i].begin(); it != adj[i].end(); it++){
+            cout << "**" <<it->first << "** Peso: " << it->second << " / ";
+        }
+        cout << endl;
+    }
+
+    int caminho2 = menor_caminho;
+    for(int i = 0; i < caminhos.size(); i++){
+        if(caminho2 == menor_caminho and i < caminhos.size()-1){
+            removerArestas(caminhos[i],caminhos[i+1]);
+            caminho2 = dijkstra(n,s,t);
+        }
+    }
+
+    cout << "Segundo menor caminho: ";
+    if(caminho2 != INF){ // verifica se foi achado um segundo caminho
+        cout << caminho2 << endl;
+    } else{
+        cout << -1 << endl;
+    }
+     cout << endl << "L.A depois: " << endl;
+
+    for(int i = 0; i< n; i++){
+        cout << "Vértice " << i << " : ";
+        for(auto it = adj[i].begin(); it != adj[i].end(); it++){
+            cout << "**" <<it->first << "** Peso: " << it->second << " / ";
+        }
+        cout << endl;
+    }
+    caminhos.clear();
+
+    return 0;
 }

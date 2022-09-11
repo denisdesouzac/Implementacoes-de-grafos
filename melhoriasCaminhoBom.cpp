7a259787161900d54dc6c688bf86850181f77cca
gZanda <gabrielfzanda@gmail.com>
2022-09-11 00:23:04 -0300

    Comentários

--- melhoriasCaminhoBom.cpp
+++ melhoriasCaminhoBom.cpp
@@ -15,10 +15,14 @@ using namespace std;
 #define INF 10000000
 #define NIL -1
 
-vector<pair<int, int>>* adj;
-vector<int> caminhos;
+vector<pair<int, int>>* adj;    // Lista de adjacência 
+vector<int> caminhos;           // Vector usado para salvar os caminhos
 
-//Função para registrar o caminho percorrido
+/*
+Função para registrar o caminho percorrido, faz isso através de uma recursão que vai "voltando" pela lista de pais
+start = vértice incial 
+end = vértice atual (onde estamos)
+*/
 void registrarCaminho(int start, int end, int pai[]){
     if(pai[start] != end){
         caminhos.push_back(pai[start]);
@@ -31,9 +35,9 @@ void registrarCaminho(int start, int end, int pai[]){
 
 // Função para realizar a remoção das arestas
 // Recebe a ultima posição do vetor de caminhos e um vetor de pais
-// Exclui o caminho até chegar ao pai da origem, ou seja quando pai[start] == -1
+// Exclui o caminho até chegar ao pai da origem, ou seja quando pai[start] == -1 (vai voltando)
 void removerArestas(int start, int pai[]){
-    while(pai[start] != NIL){//enquanto o pai da origem não for encontrado
+    while(pai[start] != NIL){   //enquanto o pai da origem não for encontrado
 
         for(int i = 0; i < int(adj[pai[start]].size()); i++){ // percorre as arestas que ligam cada vértice
 
@@ -48,7 +52,15 @@ void removerArestas(int start, int pai[]){
     }
 }
 
+// Função do algoritmo de Dijkstra 
+/*
+n = numero de vertices
+s = vertice inicial
+t = destino da busca ()
+pai[] = vetor de pais
+*/
 int dijkstra(int n, int s, int t, int pai[]){
+
     // distância de cada vértice
     int dist[n];
 
@@ -102,11 +114,11 @@ int dijkstra(int n, int s, int t, int pai[]){
             }
         }
     }
-    if(dist[t] != INF){
-        caminhos.push_back(t);
-        registrarCaminho(t,s, pai);
+    if(dist[t] != INF){     // verifica se a distância de t != INF, quer dizer que ele foi relaxado (temos caminho mais curto pra ele)
+        caminhos.push_back(t);          // Insere inicialmente o vértice final
+        registrarCaminho(t,s, pai);     // Chama a função para registrar esse MENOR CAMINHO
     }
-    return dist[t];
+    return dist[t]; // Retorna a distância do caminho mais curto
 }
 
 int main(){
@@ -115,46 +127,46 @@ int main(){
     int u, v, p; //  pontos u e v da aresta com distância p
 
     cin >> n >> m;
-     while(n != 0 and m != 0){
+    
+    while(n != 0 and m != 0){
+
         cin >> s >> t;
 
-        adj = new vector<pair<int, int> >[n];
-        int pai[n];
+        adj = new vector<pair<int, int> >[n];   // Instancia a Lista de Adjacência
+        int pai[n];                             // Vetor com os respectivos pais de cada vértice
 
         // leitura do grafo
         for(int i = 0; i < m; i++){
             cin >> u >> v >> p;
             adj[u].push_back(make_pair(v, p));
         }
-        int menor_caminho  = dijkstra(n,s,t,pai);
 
-        // inversão do caminho
+        int menor_caminho  = dijkstra(n,s,t,pai);   // Chama a função de Dijkstra
+
+        // inversão do caminho (necessária para a remoção)
         reverse(caminhos.begin(), caminhos.end());
 
         //int aux1, aux2;
         int caminho2 = menor_caminho;
         
-        if(caminho2 == INF){
+        if(caminho2 == INF){    // Se não temos menor caminho -> -1, pois sequer teremos caminho bom
             cout << -1 << endl;
         }
         else{
-            while(caminho2 == menor_caminho){
-                removerArestas(caminhos[0], pai);              
-                caminhos.clear();
-                caminho2 = dijkstra(n,s,t,pai);
+            while(caminho2 == menor_caminho){       // Enquanto esse menor caminho for igual, procura o "CAMINHO BOM"
+                removerArestas(caminhos[0], pai);   // Função para remover esse caminho ( aqui é a chave )
+                caminhos.clear();                   // Limpa o vector de caminhos, para nova busca (afinal, vamos achar novo caminho)
+                caminho2 = dijkstra(n,s,t,pai);     // Chama a busca por um novo caminho
             }
-       
-            if(caminho2 != INF){ // verifica se foi achado um segundo caminho
+        
+            if(caminho2 != INF){ // verifica se foi achado um CAMINHO BOM
                 cout <<caminho2 << endl;
             } 
             else{
                 cout << -1 << endl;
             }
-
         }
-
-        caminhos.clear();
-
+        caminhos.clear();   // Limpa o vector de caminhos
         cin >> n >> m;
     }
     return 0;
